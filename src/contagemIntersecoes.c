b15fbf16d32b2002d1525dbcf0eddd404926deb1
lucasgpulcinelli <lucasgpulcinelli@users.noreply.github.com>
2022-05-18 22:03:47 -0300

    add contagemIntersecoes

--- src/contagemIntersecoes.c
+++ src/contagemIntersecoes.c
@@ -30,7 +30,7 @@ void contagemIntersecoesArquivo(char* arq_A, char* arq_B, char* arq_out){
 }
 
 Lista* contagemIntersecoes(ListaTuples* A, ListaTuples* B){
-    Lista* l = criarListaZerada(A->tam);
+    Lista* contagens = criarListaZerada(A->tam);
 
     ordenaNumeros(A);
     ordenaNumeros(B);
@@ -38,5 +38,20 @@ Lista* contagemIntersecoes(ListaTuples* A, ListaTuples* B){
     printListaTuples(A);
     printListaTuples(B);
 
-    return l;
+    size_t primeiro_ib = 0;
+    for(size_t ia = 0; ia < A->tam; ia++){
+        for(size_t ib = primeiro_ib; ib < B->tam; ib++){
+            if(A->val[ia][1] < B->val[ib][0] || A->val[ia][0] > B->val[ib][1]){
+                if(contagens->val[ia] == 0){
+                    primeiro_ib = ib;
+                }
+            }else{
+                (contagens->val[ia])++;
+            }
+        }
+    }
+
+    printLista(contagens);
+
+    return contagens;
 }
