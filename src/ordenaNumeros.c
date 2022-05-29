526c88a31d22a957a97decc55d904c50c3ee74c5
lucasgpulcinelli <lucasgpulcinelli@users.noreply.github.com>
2022-05-29 17:04:18 -0300

    Changing ordenaNumeros to stable sorting algorthm

--- src/ordenaNumeros.c
+++ src/ordenaNumeros.c
@@ -10,34 +10,12 @@ void swap(ListaTuples* lt, size_t a, size_t b){
     lt->val[b] = tmp; 
 }
 
-size_t part(ListaTuples* lt, size_t start, size_t end){
-    int endel = lt->val[end][0];
-
-    size_t smaller_index = start;
-    for(size_t i = start; i < end; i++){
-        if(lt->val[i][0] < endel){
-            swap(lt, i, smaller_index);
-            smaller_index++;
+void ordenaNumeros(ListaTuples* lt){
+    for(size_t i = 0; i < lt->tam; i++){
+        for(size_t j = 0; j < lt->tam-i-1; j++){
+            if(lt->val[j][0] > lt->val[j+1][0]){
+                swap(lt, j, j+1);
+            }
         }
     }
-
-    swap(lt, smaller_index, end);
-    return smaller_index;
-}
-
-void quicksort(ListaTuples* lt, size_t start, size_t end){
-
-    if(start >= end){
-        return;
-    }
-
-    size_t part_index = part(lt, start, end);
-
-    quicksort(lt, start, (part_index == 0)? 0 : part_index-1);
-    quicksort(lt, part_index+1, end);
-
-}
-
-void ordenaNumeros(ListaTuples* lt){
-    quicksort(lt, 0, lt->tam-1);
 }
