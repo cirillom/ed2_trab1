5f1964003d0c21035f2bdbbeac8013de17a8cf21
lucasgpulcinelli <lucasgpulcinelli@users.noreply.github.com>
2022-06-01 15:13:36 -0300

    adicionando noticia de erro do errno em portugues

--- src/utils.c
+++ src/utils.c
@@ -2,12 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <locale.h>
 
 #include "utils.h"
 
 
 void erroFatal(int linha, char* arq, char* fmt, ...){
     int errno_inicial = errno; //errno pode mudar de valor nos printfs
+
+    //coloca a linguagem como pt_BR (para strerror sair em portugues)
+    setlocale(LC_ALL, "pt_BR.utf8");
 
     va_list ap;
 
