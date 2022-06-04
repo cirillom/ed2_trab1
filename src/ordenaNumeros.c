663bdfad4bc3fd061757f62cdc25b6d5ae465cd9
lucasgpulcinelli <lucasgpulcinelli@users.noreply.github.com>
2022-06-04 11:15:42 -0300

    Ordenação somente pela primeira coluna

--- src/ordenaNumeros.c
+++ src/ordenaNumeros.c
@@ -3,18 +3,18 @@
 #include "ordenaNumeros.h"
 #include "utils.h"
 
-void ordenaDigitos(ListaTuples* lt, int exp, int s){
+void ordenaDigitos(ListaTuples* lt, int exp){
     ListaTuples* saida = criarListaTuples(lt->tam, lt->col);
     int count[10] = { 0 };
 
     for (size_t i = 0; i < lt->tam; i++)
-        count[(lt->val[i][s] / exp) % 10]++;
+        count[(lt->val[i][0] / exp) % 10]++;
   
     for (size_t i = 1; i < 10; i++)
         count[i] += count[i - 1];
   
     for (int i = (int)lt->tam - 1; i >= 0; i--) {
-        int index = (lt->val[i][s] / exp) % 10;
+        int index = (lt->val[i][0] / exp) % 10;
         memcpy(saida->val[count[index] - 1], lt->val[i], sizeof(int)*lt->col);
         count[index]--;
     }
@@ -28,7 +28,6 @@ void ordenaDigitos(ListaTuples* lt, int exp, int s){
 void ordenaNumeros(ListaTuples* lt){
     int m = obterMax(lt);
 
-    for(int i = (int)lt->col; i >= 0; i--)
-        for (int exp = 1; m / exp > 0; exp *= 10)
-            ordenaDigitos(lt, exp, i);
+    for (int exp = 1; m / exp > 0; exp *= 10)
+        ordenaDigitos(lt, exp);
 }
